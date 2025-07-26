3d3efb425120f1d22c2a227de12b86bf7cfb39ce
afx553 <afx553@gmail.com>
2025-07-26 05:54:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-// dummy server just for SSRF shell hosting
+const express = require("express");
+const app = express();
 
+app.use(express.static("public")); // يقدم ملفات من public/
 
+app.listen(10000, () => {
+  console.log("🔥 Server running on port 10000");
+});
