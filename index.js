b2768fcd380a1da9ba07149dd57f9420a66ba5ee
afx553 <afx553@gmail.com>
2025-07-26 06:30:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
+// index.js
 const express = require("express");
 const { exec } = require("child_process");
 const app = express();
 
-app.use(express.static("public")); // لتقديم الملفات الثابتة لو تحتاج
-
 app.get("/pal", (req, res) => {
-  exec("curl http://canarytokens.com/traffic/articles/rkd5klrikb02pqoit0p4a5ngh/post.jsp", (err, stdout, stderr) => {
-    if (err) {
-      console.error("Error:", err);
-      res.status(500).send("Execution failed.");
-      return;
-    }
-    console.log("Executed:", stdout);
-    res.send("Command executed.");
+  exec("curl http://169.254.169.254", (err, stdout, stderr) => {
+    console.log("Request fired");
   });
+
+  res.send(`<h1>SSRF Canaryping</h1>`);
 });
 
-app.listen(10000, () => {
-  console.log("🔥 Server running on port 10000");
-});
+app.listen(10000, () => console.log("🔥 Listening on 10000"));
